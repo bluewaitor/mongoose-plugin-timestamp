d2e5b3fca5a6b41607225cbaddf419a79b4d2225
bluewaitor <405622394@qq.com>
2017-07-25 18:53:33 +0800

    remove comments

--- index.js
+++ index.js
@@ -11,39 +11,31 @@ module.exports = exports = function timestampPlugin(schema, options){
         }
     });
 
-
     schema.pre('save', function(next){
-        console.log('save ing ...')
         this.updated = Date.now();
         next();
     });
 
     schema.pre('update', function(next){
-        console.log('updated ing ...');
         this.update({},{ $set: { updated: Date.now() } });
         next();
     });
 
     schema.pre('updateMany', function(next){
-        console.log('updateMany ing ...');
         this.update({},{ $set: { updated: Date.now() } });
         next();
     });
     
     schema.pre('updateOne', function(next){
-        console.log('updateOne ing ...');
         this.update({},{ $set: { updated: Date.now() } });
         next();
     });
-
 
     schema.pre('findOneAndUpdate', function(next){
         console.log('findOneAndUpdate ing ...');
         this.update({},{ $set: { updated: Date.now() } });
         next();
     });
-
-    
 
     if (options && options.index) {
         schema.path('updated').index(options.index)
